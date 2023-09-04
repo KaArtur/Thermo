de6b2453fa5a30203311523393b72037ca4e621f
Artur <arturkawa111@gmail.com>
2023-09-04 15:19:10 +0200

    validation client

--- src/main/java/pl/thermo/thermo/controller/ClientController.java
+++ src/main/java/pl/thermo/thermo/controller/ClientController.java
@@ -1,7 +1,9 @@
 package pl.thermo.thermo.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +27,10 @@ public class ClientController {
     }
 
     @PostMapping("/saveclient")
-    public String saveClient(@ModelAttribute("client") ClientModel clientModel) {
+    public String saveClient(@Valid @ModelAttribute("client") ClientModel clientModel, BindingResult bindingResult) {
+       if (bindingResult.hasErrors()) {
+           return "clientPage";
+       }
         clientService.saveClient(clientModel);
         return "redirect:/client";
     }
